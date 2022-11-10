857cb8d5fc2f034ccd87d1eb3af12c341d9d686f
Alex Ryan <alex@chalk.ai>
2022-11-09 22:37:21 -0800

    Add more integ tests

--- src/__test__/integration.test.ts
+++ src/__test__/integration.test.ts
@@ -6,29 +6,51 @@ interface FraudTemplateFeatures {
   "user.socure_score": number;
 }
 
-const maybe = Boolean(process.env.CHALK_INTEGRATION)
-  ? describe
-  : describe.skip;
+const maybe = Boolean(process.env.CHALK_INTEGRATION) ? describe : describe.skip;
 
 maybe("integration tests", () => {
-  it("query fraud-template user.id=1", async () => {
-    console.log(
-      "clientId",
-      (process.env.CI_CHALK_CLIENT_ID ?? "").substring(0, 2)
-    );
-
-    const client = new ChalkClient<FraudTemplateFeatures>({
+  let client: ChalkClient;
+  beforeAll(() => {
+    client = new ChalkClient<FraudTemplateFeatures>({
       clientId: process.env.CI_CHALK_CLIENT_ID,
       clientSecret: process.env.CI_CHALK_CLIENT_SECRET,
     });
+  });
 
-    const result = await client.query({
-      inputs: {
-        "user.id": 1,
-      },
-      outputs: ["user.id"],
+  describe("query fraud-template user.id=1", () => {
+    it("query user.id", async () => {
+      const result = await client.query({
+        inputs: {
+          "user.id": 1,
+        },
+        outputs: ["user.id"],
+      });
+
+      expect(result.data["user.id"].value).toBe(1);
     });
 
-    expect(result.data["user.id"]).toBe(1);
+    it("query user.gender", async () => {
+      const result = await client.query({
+        inputs: {
+          "user.id": 1,
+        },
+        outputs: ["user.gender"],
+      });
+
+      expect(result.data["user.gender"].value).toBe("m");
+    });
+
+    it("query user.id and user.gender", async () => {
+      const result = await client.query({
+        inputs: {
+          "user.id": 2,
+        },
+        outputs: ["user.id", "user.gender"],
+      });
+
+      expect(Object.keys(result.data).length).toBe(2);
+      expect(result.data["user.id"].value).toBe("2");
+      expect(result.data["user.gender"].value).toBe("f");
+    });
   });
 });
