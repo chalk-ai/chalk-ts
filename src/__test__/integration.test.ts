ce3944ea53e82b580a978362e7802aba29864d2e
Alex Ryan <alex@chalk.ai>
2022-11-09 22:23:45 -0800

    Don't run integration tests locally

--- src/__test__/integration.test.ts
+++ src/__test__/integration.test.ts
@@ -6,8 +6,14 @@ interface FraudTemplateFeatures {
   "user.socure_score": number;
 }
 
-describe("integration", () => {
+const maybe = Boolean(process.env.CHALK_INTEGRATION_TESTS)
+  ? describe
+  : describe.skip;
+
+maybe("integration", () => {
   it("query fraud-template user.id=1", async () => {
+    console.log('clientId', process.env.CHALK_CLIENT_ID);
+
     const client = new ChalkClient<FraudTemplateFeatures>({
       clientId: process.env.CHALK_CLIENT_ID,
       clientSecret: process.env.CHALK_CLIENT_SECRET,
