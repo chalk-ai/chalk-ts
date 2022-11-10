15d427f516357e327d1e62a87d5eb5bd7dea1435
Alex <113044905+alex-chalk@users.noreply.github.com>
2022-11-09 22:45:11 -0800

    Add integration test (#6)

---
+++ src/__test__/integration.test.ts
@@ -0,0 +1,56 @@
+import { ChalkClient } from "../_client";
+
+interface FraudTemplateFeatures {
+  "user.id": number;
+  "user.gender": "m" | "f" | "x";
+  "user.socure_score": number;
+}
+
+const maybe = Boolean(process.env.CHALK_INTEGRATION) ? describe : describe.skip;
+
+maybe("integration tests", () => {
+  let client: ChalkClient;
+  beforeAll(() => {
+    client = new ChalkClient<FraudTemplateFeatures>({
+      clientId: process.env.CI_CHALK_CLIENT_ID,
+      clientSecret: process.env.CI_CHALK_CLIENT_SECRET,
+    });
+  });
+
+  describe("query fraud-template", () => {
+    it("query user.id", async () => {
+      const result = await client.query({
+        inputs: {
+          "user.id": 1,
+        },
+        outputs: ["user.id"],
+      });
+
+      expect(result.data["user.id"].value).toBe(1);
+    });
+
+    it("query user.gender", async () => {
+      const result = await client.query({
+        inputs: {
+          "user.id": 1,
+        },
+        outputs: ["user.gender"],
+      });
+
+      expect(result.data["user.gender"].value).toBe("f");
+    });
+
+    it("query user.id and user.gender", async () => {
+      const result = await client.query({
+        inputs: {
+          "user.id": 2,
+        },
+        outputs: ["user.id", "user.gender"],
+      });
+
+      expect(Object.keys(result.data).length).toBe(2);
+      expect(result.data["user.id"].value).toBe(2);
+      expect(result.data["user.gender"].value).toBe("f");
+    });
+  });
+});
