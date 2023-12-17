bb3cc513ff19660c121126c5df71c15fef1ce9d0
Elliot Marx <elliot@chalk.ai>
2023-12-17 09:12:11 -0800

    Add docs to query interface

--- src/_interface.ts
+++ src/_interface.ts
@@ -16,24 +16,58 @@ export interface ChalkOnlineQueryRequest<
   TFeatureMap,
   TOutput extends keyof TFeatureMap
 > {
+  // The features for which there are known values, mapped to those values.
+  // For example, `{'user.id': 1234}`.
   inputs: Partial<TFeatureMap>;
+
+  // Outputs are the features that you'd like to compute from the inputs.
+  // For example, `['user.age', 'user.name', 'user.email']`.
   outputs: TOutput[];
+
+  // Maximum staleness overrides for any output features or intermediate features.
+  // See https://docs.chalk.ai/docs/query-caching for more information.
   staleness?: {
     [K in keyof TFeatureMap]?: string;
   };
+
   scopeTags?: string[];
+
+  // If specified, Chalk will route your request to the relevant preview deployment.
   previewDeploymentId?: string;
+
+  // You can specify a correlation ID to be used in logs and web interfaces.
+  // This should be globally unique, i.e. a `uuid` or similar. Logs generated
+  // during the execution of your query will be tagged with this correlation id.
   correlationId?: string;
+
+  // The semantic name for the query you're making, for example, `"loan_application_model"`.
+  // Typically, each query that you make from your application should have a name.
+  // Chalk will present metrics and dashboard functionality grouped by 'query_name'.
   queryName?: string;
+
   queryMeta?: {
     [key: string]: string;
   };
+
   encodingOptions?: {
     encodeStructsAsObjects?: boolean;
   };
 
   // `now` should be an ISO-formatted "zoned datetime" or "instant" string.
+  // The time at which to evaluate the query. If not specified, the current time will be used.
+  // This parameter is complex in the context of online_query since the online store
+  // only stores the most recent value of an entity's features. If `now` is in the past,
+  // it is extremely likely that `None` will be returned for cache-only features.
+  //
+  // This parameter is primarily provided to support:
+  // - controlling the time window for aggregations over cached has-many relationships
+  // - controlling the time wnidow for aggregations over has-many relationships loaded from an
+  // external database
+  //
+  // If you are trying to perform an exploratory analysis of past feature values, prefer `offline_query`.
   now?: string;
+
+  include_meta?: boolean;
 }
 
 export type ChalkOnlineQueryResponseStatusKind =
