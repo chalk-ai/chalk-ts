0df45029b0d7b3acedb41450ae34fd4bf93a2e79
John Rotter <jrotter@chalk.ai>
2025-07-31 17:15:06 -0400

    add metadata_val

--- src/_interface/index.ts
+++ src/_interface/index.ts
@@ -178,6 +178,10 @@ export interface ChalkOnlineQueryFeatureResult<
   // If an error occurred resolving this feature, this field will be 'false'.
   valid?: boolean;
 
+  // Value of the internal metadata that encodes information about how this feature
+  // was computed.
+  metadata_val?: number;
+
   // Only included if `include_meta` is true.
   meta?: ChalkFeatureMeta;
 }
@@ -186,6 +190,8 @@ export interface ChalkOnlineQueryResponse<
   TFeatureMap,
   TOutput extends keyof TFeatureMap
 > {
+
+
   // The output features and any query metadata.
   data: {
     [K in TOutput]: ChalkOnlineQueryFeatureResult<TFeatureMap, K>;
