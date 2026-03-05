5024043eb5bd8ee498f1b96f43c90bdac6a82241
kelvin-chalk <kelvin@chalk.ai>
2026-03-04 18:17:23 -0800

    Fix: Add UInt64, LargeUtf8, and Float64 types to supported 64 bit types (#86)
    * add UInt64, LargeUtf8, and Float64 types to supported 64 bit types
    
    * fix test

--- src/__test__/serialization.test.ts
+++ src/__test__/serialization.test.ts
@@ -35,7 +35,7 @@ describe("featherRequestHeaderFromBody", () => {
       feather_body_type: "RECORD_BATCHES",
       response_compression_scheme: "uncompressed",
       client_supports_64bit: false,
-      client_supported_64bit_types: ["Int64", "LargeBinary"],
+      client_supported_64bit_types: ["Int64", "LargeBinary", "UInt64", "LargeUtf8", "Float64"],
     });
   });
 
